3f95805eec374f3a45491181771d7e1dfd2ee21f
Subhash <Subhash>
2016-01-20 18:53:36 +0530

    newcommittttttt

--- havas-scripts/restart.sh
+++ havas-scripts/restart.sh
@@ -1,23 +1,2 @@
 !/bin/bash
-RESTART="/sbin/service httpd restart"
-
-#dddddddddddddddddddddddd
-# uncomment if you are using Debian / Ubuntu Linux
-#RESTART="/etc/init.d/httpd -k restart"
-
-#path to pgrep command
-PGREP="/usr/bin/pgrep"
-
-# Httpd daemon name,
-# Under RHEL/CentOS/Fedora it is httpd
-# Under Debian 4.x it is apache2
-HTTPD="httpd"
-
-# find httpd pid
-$PGREP ${HTTPD}
-
-#if [ $? -ne 0 ] # if apache not running
-#then
-# restart apache
-$RESTART
-#fi
+sudo /sbin/service httpd restart
